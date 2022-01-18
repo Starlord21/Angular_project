fb0e64f17385c6291562d63eb1df23c0f85860f6
Starlord21 <92169285+Starlord21@users.noreply.github.com>
2022-01-18 18:21:17 +0530

    heroku app

--- test/API/app.js
+++ test/API/app.js
@@ -2,12 +2,31 @@ var express =require("express")
 var app = express()
 var cors = require("cors")
 
+
+const MONGODB_URI = "mongodb+srv://kartik:90129012@cluster0.srf2f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const url = `mongodb+srv://sample_user:<password>@my-sample-cluster-b3ugy.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+
+
+
+ 
+
+app.use(express.static(__dirname+"/assets"))
 app.use(express.json())
 app.use(express.urlencoded())
+// app.use(cors(corsOptions))
 app.use(cors())
 
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
 app.use(require("./config/routes"))
+app.get("*",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
 
+
+
+ 
 var port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log("server running with", port)
--- test/API/config/database.js
+++ test/API/config/database.js
@@ -1,6 +1,8 @@
 module.exports= 
 {
-    dbUrl : "mongodb://localhost:27017",
+    dbUrl : "mongodb+srv://kartik:90129012@cluster0.srf2f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     dbName : "test",
-    encryptStr : "guardian of galaxy"
+    encryptStr : "guardian of galaxy",
+   
+
 }
